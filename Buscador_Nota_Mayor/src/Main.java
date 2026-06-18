e64d4f8f04c54530e2243c6f6dd6cf28131dbe8e
ShinobiDev79 <292239799+ShinobiDev79@users.noreply.github.com>
2026-06-18 18:08:36 +0200

    Refactorización de la variable cont y cierre del recurso

--- Buscador_Nota_Mayor/src/Main.java
+++ Buscador_Nota_Mayor/src/Main.java
@@ -7,13 +7,11 @@ public class Main {
         Scanner lector = new Scanner(System.in);
         double[] notas = new double[5];
         double notaMayor = 0;
-        int cont = 0;
 
         //Paso 2: Petición al usuario de introducir las notas para evaluar.
-        for (int i = 1; i <= 5; i++) {
-            System.out.printf("Introduce la nota %d: ", i);
-            notas[cont] = lector.nextDouble();
-            cont += 1;
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("Introduce la nota %d: ", i + 1);
+            notas[i] = lector.nextDouble();
         }
 
         //Paso 3: Evaluamos las notas ingresadas para determinar la mayor nota.
@@ -25,5 +23,8 @@ public class Main {
 
         //Paso 4: Se imprime por pantalla la nota mayor de las introducidas por el usuario.
         System.out.printf("La nota más alta de la clase es: %.2f", notaMayor);
+
+        //Paso 5: Recurso cerrado con éxito.
+        lector.close();
     }
 }
