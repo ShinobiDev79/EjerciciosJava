787fdf3f92ccfa7bb1893e7606eab610abd47b45
ShinobiDev79 <292239799+ShinobiDev79@users.noreply.github.com>
2026-06-22 21:57:36 +0200

    Se mejora el ejercicio Creador de personajes RPG, interactuando con el objeto

--- Creador_Personajes_RPG/src/Main.java
+++ Creador_Personajes_RPG/src/Main.java
@@ -2,10 +2,22 @@ public class Main {
     public static void main(String[] args) {
 
         //Creamos la instancia heroe dándole un nombre y un nivel que son los atributos necesarios.
-        Personaje heroe = new Personaje("Aragorn", 1);
+        Personaje montaraz = new Personaje("Aragorn", 1, 100.0);
 
-        //A través de esta función imprimiremos por pantalla los datos del personaje.
-        heroe.mostrarInfo();
+        //A través de esta función imprimiremos por pantalla los datos iniciales del personaje.
+        montaraz.mostrarInfo();
+
+        //Con esta función, el personaje recibirá daño.
+        montaraz.recibirDanio(35.5);
+
+        //Volvemos a mostrar la información del personaje después del daño recibido.
+        montaraz.mostrarInfo();
+
+        //Con esta función, el personaje sube un nivel tras ganar el combate.
+        montaraz.subirNivel();
+
+        //Al final mostramos los datos actualizados del personaje.
+        montaraz.mostrarInfo();
 
     }
 }
--- Creador_Personajes_RPG/src/Personaje.java
+++ Creador_Personajes_RPG/src/Personaje.java
@@ -2,16 +2,35 @@ public class Personaje {
     //Atributos de la clase.
     String nombre;
     int nivel;
+    double puntosVida;
 
     //Constructor de la clase.
-    public Personaje(String nombreRecibido, int nivelRecibido){
-        nombre = nombreRecibido;
-        nivel = nivelRecibido;
+    public Personaje(String nombre, int nivel, double puntosVida){
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.puntosVida = puntosVida;
     }
 
     //Función para imprimir los datos del personaje.
     public void mostrarInfo(){
         System.out.println("Nombre: " + nombre);
         System.out.println("Nivel: " + nivel);
+        System.out.println("Puntos de vida: " + puntosVida);
+        System.out.println(" ");
+    }
+
+    //Función al recibir daño el personaje.
+    public void recibirDanio (double cantidad){
+        puntosVida = puntosVida - cantidad;
+        System.out.printf("%s, ha recibido %.2f punto de daño!", nombre, cantidad);
+        System.out.println(" ");
+    }
+
+    //Función al subir de nivel el personaje.
+    public void subirNivel(){
+        nivel += 1;
+        puntosVida = 100;
+        System.out.printf("%s ha subido al nivel %d y se restauró su vida.", nombre, nivel);
+        System.out.println(" ");
     }
 }
